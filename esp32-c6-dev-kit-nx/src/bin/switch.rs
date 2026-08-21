9214e902998cc6a33810502373e9206b7d0f5345
Aditya Grewal <aditya1998yr@gmail.com>
2026-08-21 15:29:21 +0200

    Add esp32c6 new tree@

---
+++ esp32-c6-dev-kit-nx/src/bin/switch.rs
@@ -0,0 +1,31 @@
+  #![no_std]
+#![no_main]
+ 
+use panic_rtt_target as _;
+use esp_hal::{
+    delay::Delay,
+    gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull},
+    main,
+};
+
+esp_bootloader_esp_idf::esp_app_desc!();
+ 
+#[main]
+fn main() -> ! {
+    rtt_target::rtt_init_defmt!();
+    
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    let mut led = Output::new(peripherals.GPIO7, Level::Low, OutputConfig::default());
+    let button = Input::new(peripherals.GPIO9, InputConfig::default().with_pull(Pull::Up));
+    let delay = Delay::new();
+ 
+    loop {
+        if button.is_low() {
+            led.set_high();
+            // delay.delay_millis(20); // simple software debounce
+        } else {
+            led.set_low();
+            }
+        delay.delay_millis(20);
+    }
+}
