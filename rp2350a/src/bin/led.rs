de40f89dc7c1f255304fc584e536e61ad678d2f3
Aditya Grewal <aditya1998yr@gmail.com>
2026-09-09 21:06:24 +0200

    rp2350a - Add blinky led blink logic

--- rp2350a/src/bin/led.rs
+++ rp2350a/src/bin/led.rs
@@ -3,9 +3,16 @@
 
 use cyw43::aligned_bytes;
 use cyw43_pio::{PioSpi, RM2_CLOCK_DIVIDER};
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Level, Output};
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
     let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw, nvram).await;
     spawner.spawn(unwrap!(cyw43_task(runner)));
+    let delay = Duration::from_millis(250);
+    loop {
+        info!("led on!");
+        control.gpio_set(0, true).await;
