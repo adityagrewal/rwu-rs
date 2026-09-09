13af38857a6497d496f437b5accbbd717a2294f6
Aditya Grewal <aditya1998yr@gmail.com>
2026-09-09 21:05:07 +0200

    rp2350a - Add blinky initial skeleton

---
+++ rp2350a/src/bin/led.rs
@@ -0,0 +1,11 @@
+#![no_std]
+#![no_main]
+
+use cyw43::aligned_bytes;
+use cyw43_pio::{PioSpi, RM2_CLOCK_DIVIDER};
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
+    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw, nvram).await;
+    spawner.spawn(unwrap!(cyw43_task(runner)));
