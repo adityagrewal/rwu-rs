11faf49fd9bdcb5767644e8c9fc097695f32b18e
Aditya Grewal <aditya1998yr@gmail.com>
2026-09-16 13:30:41 +0200

    rp2350a - Add peripheral init switch.rs

--- rp2350a/src/bin/switch.rs
+++ rp2350a/src/bin/switch.rs
@@ -6,3 +6,9 @@ use embassy_rp::gpio::{Input, Level, Output, Pull};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    let mut led = Output::new(p.PIN_15, Level::Low);
+    let mut button = Input::new(p.PIN_16, Pull::Up);
+
