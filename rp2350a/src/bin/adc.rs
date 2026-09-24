b82d35245a9ff7533ea40e5201dc017524e1dfc6
Aditya Grewal <aditya1998yr@gmail.com>
2026-09-24 20:27:43 +0200

    rp2350a - ADC start main fn

--- rp2350a/src/bin/adc.rs
+++ rp2350a/src/bin/adc.rs
@@ -12,3 +12,8 @@ use {defmt_rtt as _, panic_probe as _};
 bind_interrupts!(struct Irqs {
     ADC_IRQ_FIFO => InterruptHandler;
 });
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    let mut adc = Adc::new(p.ADC, Irqs, Config::default());
