afef2620277d35ed9cf6436986470bf72d6eb5d1
Aditya Grewal <aditya1998yr@gmail.com>
2026-09-24 20:25:56 +0200

    rp2350 - ADC add dependencies

---
+++ rp2350a/src/bin/adc.rs
@@ -0,0 +1,10 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::adc::{Adc, Channel, Config, InterruptHandler};
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::Pull;
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
