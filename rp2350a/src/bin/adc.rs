3eb1598b3b046479efc441835e63247353d826a1
Aditya Grewal <aditya1998yr@gmail.com>
2026-09-25 21:31:55 +0200

    rp2350a - ADC temp API

--- rp2350a/src/bin/adc.rs
+++ rp2350a/src/bin/adc.rs
@@ -33,3 +33,9 @@ async fn main(_spawner: Spawner) {
         let temp = adc.read(&mut ts).await.unwrap();
         info!("Temp: {} degrees", convert_to_celsius(temp));
         Timer::after_secs(1).await;
+fn convert_to_celsius(raw_temp: u16) -> f32 {
+    let temp = 27.0 - (raw_temp as f32 * 3.3 / 4096.0 - 0.706) / 0.001721;
+    let sign = if temp < 0.0 { -1.0 } else { 1.0 };
+    let rounded_temp_x10: i16 = ((temp * 10.0) + 0.5 * sign) as i16;
+    (rounded_temp_x10 as f32) / 10.0
+}
