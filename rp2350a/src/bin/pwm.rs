785d07f3d25010fb7f6ff6ec11bfca8e53d2598d
Aditya Grewal <aditya1998yr@gmail.com>
2026-09-25 21:34:33 +0200

    rp2350a - PWM add async main

--- rp2350a/src/bin/pwm.rs
+++ rp2350a/src/bin/pwm.rs
@@ -8,3 +8,10 @@ use embassy_rp::peripherals::{PIN_4, PIN_15, PWM_SLICE2, PWM_SLICE7};
 use embassy_rp::pwm::{Config, Pwm, SetDutyCycle};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    spawner.spawn(pwm_set_config(p.PWM_SLICE7, p.PIN_15).unwrap());
+    spawner.spawn(pwm_set_dutycycle(p.PWM_SLICE2, p.PIN_4).unwrap());
+}
